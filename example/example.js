f98ed89987089014bb08d217b813175f1b1cca47
Jaime <jaimebeneytezduran@gmail.com>
2016-01-26 09:40:44 +0000

    chore(Example): Example file updated

--- example/example.js
+++ example/example.js
@@ -5,21 +5,7 @@ angular.module('ng.cx.grid.example', ['ng.cx.grid'])
     function($scope) {
         'use strict';
 
-        var _columnHeders = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-            _rowHeders = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'],
-            _rawMatrix = [
-                ['a0', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9'],
-                ['b0', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'b9'],
-                ['c0', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9'],
-                ['d0', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9'],
-                ['e0', 'e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8', 'e9'],
-                ['f0', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9'],
-                ['g0', 'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9'],
-                ['h0', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9'],
-                ['i0', 'i1', 'i2', 'i3', 'i4', 'i5', 'i6', 'i7', 'i8', 'i9'],
-                ['j0', 'j1', 'j2', 'j3', 'j4', 'j5', 'j6', 'j7', 'j8', 'j9']
-            ],
-            _matrix = [];
+        var _columnHeders = [], _rowHeders = [], _matrix = [];
 
         _init();
 
@@ -33,11 +19,19 @@ angular.module('ng.cx.grid.example', ['ng.cx.grid'])
         }
 
         function _initializeMatrix() {
-            _matrix = _rawMatrix.map(function(row) {
-                return row.map(itemParser);
-            });
-            _rowHeders = _rowHeders.map(itemParser);
-            _columnHeders = _columnHeders.map(itemParser);
+            var width = 15,
+                height = 15;
+
+            for (var row = 0; row < width; row++) {
+                _rowHeders.push(itemParser('r-' + row));
+                _matrix.push([]);
+                for (var col = 0; col < height; col++) {
+                    if(row===0) {
+                        _columnHeders.push(itemParser('c-' + col));
+                    }
+                    _matrix[row].push(itemParser(row + '-' + col));
+                }
+            }
         }
 
         function itemParser (item) {
